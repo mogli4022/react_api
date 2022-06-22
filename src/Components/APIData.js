6cd66e448720a4dd35666d54a405112cde0119b1
mogli4022 <mogli4022@gmail.com>
2022-06-22 10:43:34 +0530

    initial commit

--- src/Components/APIData.js
+++ src/Components/APIData.js
@@ -3,10 +3,13 @@ import { useEffect, useState } from 'react';
 function APIData() {
 
     const [data, setData] = useState([])
-    const [count, setCount] = useState(0)
+    const [notesData, setnotesData] = useState({
+        title:'',
+        content:''
+    })
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        axios.get("http://localhost:5000/notes")
         .then((response) => {
             setData(response.data)
         })
@@ -14,22 +17,49 @@ function APIData() {
             console.log(e);
         })
     },[])
+
+    // Handle change
+
+    const handleChange = (e) => {
+        setnotesData({
+            ...notesData,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const addNotes = async () => {
+        await axios.post("http://localhost:5000/notes", notesData)
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((e)=> {
+            console.log(e);
+        })
+        console.log(data);
+    }
+
     return ( 
         <>
-        {/* <p>
-            {data[0]}
-        </p> */}
+
+    <div>
+        <h3>title:</h3>
+        <input type="text" name="title" value={notesData.title} onChange={handleChange}/>
+        <h3>content:</h3>
+        <input type="text" name="content" value={notesData.content} onChange={handleChange}/>
+        <div style={{marginTop:"15px"}}><button onClick={addNotes}>Add</button></div>
+    </div>
         {
-            data.map((value, index) => {
-               return(
-               <div  key={index}>
-                <h1>{value.id}</h1>
-                 <h3>{value.title}</h3>
-                <p>{value.body}</p>
-               </div>
-               )
+            data.map((value) => {
+                return(
+                    <div key={value._id}>
+                        <h1>{value.title}</h1>
+                        <p>{value.content}</p>
+                    </div>
+                )
             })
         }
+
+
         </>
      );
 }
